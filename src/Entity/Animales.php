eab810ae1b834c0ffeee8c8fa4f37af0a2276341
ePayco <sergio.rios@payco.co>
2020-03-08 16:44:38 -0500

    se creo una tabla en la base de datos y luego se importo, tambien se hizo el proceso de crear la entidad y migrarla a la base de datos (se realizaron los 2 procesos con dos tablas diferentes)

---
+++ src/Entity/Animales.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Animales
+ *
+ * @ORM\Table(name="animales")
+ * @ORM\Entity
+ */
+class Animal
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="tipo", type="string", length=200, nullable=true)
+     */
+    private $tipo;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="color", type="string", length=200, nullable=true)
+     */
+    private $color;
+
+    /**
+     * @var string|null
+     *
+     * @ORM\Column(name="raza", type="string", length=255, nullable=true)
+     */
+    private $raza;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTipo(): ?string
+    {
+        return $this->tipo;
+    }
+
+    public function setTipo(?string $tipo): self
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(?string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+
+    public function getRaza(): ?string
+    {
+        return $this->raza;
+    }
+
+    public function setRaza(?string $raza): self
+    {
+        $this->raza = $raza;
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ */
+class User
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=200, nullable=true)
+     */
+    private $apellidos;
+
+    /**
+     * @ORM\Column(type="string", length=200)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getApellidos(): ?string
+    {
+        return $this->apellidos;
+    }
+
+    public function setApellidos(?string $apellidos): self
+    {
+        $this->apellidos = $apellidos;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
