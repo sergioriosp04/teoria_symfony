6a3a5a916cd26fe33a70d57a972bf11969cc8bda
ePayco <sergio.rios@payco.co>
2020-03-08 13:30:39 -0500

    metodo de pruebas al homecontroller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="home")
+     */
+    public function index()
+    {
+        $json=[
+            "hola"=>"hola"
+        ];
+        return $this->json($json);
+        /*return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+            'hello' => 'hola mundo'
+        ]);*/
+    }
+
+    /**
+     * @Route("/home/animales/{name?sergio}", name="animales", methods={"POST"})
+     */
+    public function animals($name){
+        $title = 'bienvenido a la pagina de animales';
+        return $this->render('home/animales.html.twig',[
+            'title' => $title,
+            'name' => $name
+        ]);
+    }
+}
